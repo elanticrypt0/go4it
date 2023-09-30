5eef5444e9ab4a044fab02db61b15c3564c0328a
elanticrypt0 <elanticrypt0@gmail.com>
2023-09-29 23:40:17 -0300

    updated set & get PrimaryDB

--- app.go
+++ app.go
@@ -1,6 +1,39 @@
 package go4it
 
+import "gorm.io/gorm"
+
 type App struct {
-	Config *AppConfig
-	DB     []DBActive
+	Config    *AppConfig
+	DB        []DBActive
+	DBPrimary *gorm.DB
+}
+
+func LoadAppConfig(configFile string) *AppConfig {
+
+	if configFile == "" {
+		configFile = "appconfig"
+	}
+	configFile = configFile + ".toml"
+
+	var appconfig AppConfig
+	ReadOrParseToml(configFile, &appconfig)
+	return &appconfig
+}
+
+func NewApp(configFile string) App {
+	return App{
+		Config: LoadAppConfig(configFile),
+	}
+}
+
+func (a *App) Connect2Db(connName string) {
+	Connect2DB(a, connName)
+}
+
+func (a *App) SetPrimaryDB(index uint8) {
+	a.DBPrimary = a.DB[index].Conn
+}
+
+func (a *App) GetPrimaryDB() *gorm.DB {
+	return a.DBPrimary
 }
