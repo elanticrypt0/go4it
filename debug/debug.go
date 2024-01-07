484e1b6479e396a437cef31aa00714be34ebf115
K23 <hashersmasher@proton.me>
2024-01-07 10:10:33 -0300

    new tools!

---
+++ debug/debug.go
@@ -0,0 +1,49 @@
+package debug
+;
+import (
+	"fmt"
+	"reflect"
+)
+
+func separator() {
+	fmt.Println("--------------------------------")
+}
+
+func debugBannerTop() {
+	fmt.Println("")
+	separator()
+	fmt.Println("> DEBUG: ")
+}
+
+func debugBannerBottom() {
+	separator()
+	fmt.Println("")
+}
+
+func String(name, value string) {
+
+	debugBannerTop()
+	log.Printf("%s: %s \n", name, value)
+	debugBannerBottom()
+
+}
+
+func Struct(name string, input interface{}) {
+
+	value := reflect.ValueOf(input)
+	numFields := value.NumField()
+
+	debugBannerTop()
+	log.Println("Name: ", name)
+	fmt.Printf("Number of fields: %d\n", numFields)
+
+	structType := value.Type()
+
+	for i := 0; i < numFields; i++ {
+		field := structType.Field(i)
+		fieldValue := value.Field(i)
+
+		fmt.Printf("Field %d: %s (%s) = %v\n", i+1, field.Name, field.Type, fieldValue)
+	}
+	debugBannerBottom()
+}
