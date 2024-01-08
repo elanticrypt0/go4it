87ff4104ebe6e5ebe118c0718c82f0a75114f40d
K23 <hashersmasher@proton.me>
2024-01-08 16:58:56 -0300

    fix model error \ add generic erro

---
+++ errors/general.go
@@ -0,0 +1,12 @@
+package errors
+
+import "fmt"
+
+type GenericError struct {
+	Message string
+	Code    int
+}
+
+func (gerr *GenericError) Error() string {
+	return fmt.Sprintf("(CODE: %d) err %s", gerr.Code, gerr.Message)
+}
