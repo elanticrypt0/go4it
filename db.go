2fad84a0621c1bea3711aac2080219595b12a239
K23 <hashersmasher@proton.me>
2024-02-24 20:02:05 -0300

    add set auth and security db connection

--- db.go
+++ db.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type DB struct {
 	Primary   *gorm.DB
 	Secondary *gorm.DB
+	Security  *gorm.DB
+	Auth      *gorm.DB
 	Actives   []DBActive
 }
 
@@ -14,4 +16,14 @@ func (b *DB) SetPrimaryDB(index uint8) {
 
 func (b *DB) SetSecondaryDB(index uint8) {
 	b.Secondary = b.Actives[index].Conn
+}
+
+// Set the db to interact with security
+func (b *DB) SetSecurityDB(index uint8) {
+	b.Security = b.Actives[index].Conn
+}
+
+// Set the db to interact with authentication
+func (b *DB) SetAuthDB(index uint8) {
+	b.Auth = b.Actives[index].Conn
 }
